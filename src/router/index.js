65b3508d7e803483b66543db750dbc91876b8c3b
731010527@qq.com <731010527@qq.com>
2017-09-26 19:33:55 +0800

    门票组件完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Home from '@/pages/home'
-
+import Ticket from '../pages/ticket/index'
 export default class RouterMap extends Component {
   render() {
     return (
@@ -10,6 +10,7 @@ export default class RouterMap extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}/>
+          <Route exact path="/ticket" component={Ticket}/>
         </Switch>
       </Router>
     </div>
