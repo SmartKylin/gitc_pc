023e2e2a4c2bc5eabed56d4124b20cc3ca6c7adc
GayeChen <1580850542@qq.com>
2017-09-26 19:29:43 +0800

    大会专题基本完成

--- src/pages/home/topics/components/TopicItem.js
+++ src/pages/home/topics/components/TopicItem.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react'
 import './index.scss'
-// import MyIcon from './MyIcon'
-
+import $ from 'jquery'
 
 export default class extends Component {
   constructor (props) {
     super(props)
   }
+  componentDidMount () {
+    this.item.onmouseenter = () => {
+      this.item.classList.add('active')
+    }
+    /*$('.topic--item').mouseenter(() => {
+      $(this).addClass('active')
+    })*/
+    this.item.onmouseleave = () => {
+      this.item.classList.remove('active')
+    }
+  }
   render () {
-    console.log(this.props.iconClass);
     return (
-      <div className='topic--item'>
-        <div className='topic--icon'>
-          {/*<i className='iconfont' style={{fontSize: '40px'}}>&#xe60b;</i>*/}
-          <i className={'iconfont ' + this.props.iconClass} style={{fontSize: '40px'}}></i>
-        </div>
+    <div className='topic--item' ref={item => this.item = item}>
+      <div className='topic--icon'>
+        <i className={'iconfont ' + this.props.iconClass} style={{fontSize: '40px'}}></i>
       </div>
+      <div className='topic--item--title'>{this.props.title}</div>
+      <div className='topic--item--content'>{this.props.content}</div>
+    </div>
     )
+  
+  }
+  componentWillUnmount () {
+    this.item.onmouseenter = null
+    this.item.onmouseleave = null
   }
 }
