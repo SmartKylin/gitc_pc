ad8f4818430ad237213adddc234593f9ba780059
wj15510151110 <wj15510151110@gmail.com>
2017-09-27 20:46:12 +0800

    修改主主页面

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
     return (
       
       
-      <div>
+      <div style={{color: '#fff'}}>
         <Strengths/>
         <Topics/>
         <Agenda/>
