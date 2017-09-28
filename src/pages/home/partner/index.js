a5ff9a948ab013ff2ced8a2b19a263da6bfbe56d
GayeChen <1580850542@qq.com>
2017-09-28 10:24:27 +0800

    合作伙伴模块完成

--- src/pages/home/partner/index.js
+++ src/pages/home/partner/index.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 import './index.scss'
+import mudushipin from '../../../images/目睹视频_28.png'
+import baige from '../../../images/百格logo_31.png'
+import hudongba from '../../../images/互动吧_33.png'
+
+import {
+  mudulink,
+  hudonglink,
+  baigelink
+} from "../partnerLink";
 
 export default class extends Component {
   render () {
     return (
       <div className='cooperation--partner'>
         <div className="partner--title">合作伙伴</div>
-        <div className="super--sponsor--area">
+        {/*<div className="super--sponsor--area">
           <div>顶级赞助</div>
         </div>
         <div className="area diamon--area">
@@ -23,19 +32,27 @@ export default class extends Component {
         </div>
         <div className="area special--area">
           <div>专项赞助</div>
+        </div>*/}
+        <div className="area live--official--area">
+          <div>视频官方直播平台</div>
+          <div className='link--group'>
+            <a href={mudulink}><img src={mudushipin} alt=""/></a>
+          </div>
         </div>
         <div className='area ticket--official--area'>
           <div>票务官网合作平台</div>
+          <div className='link--group'>
+            <a href={baigelink}><img src={baige} alt=""/></a>
+            <a href={hudonglink}><img src={hudongba} alt=""/></a>
+          </div>
         </div>
-        <div className='area ticket--area'>
+       {/* <div className='area ticket--area'>
           <div>票务官网合作平台</div>
         </div>
-        <div className="area live--official--area">
-          <div>视频官方直播平台</div>
-        </div>
+       
         <div className="area live--area">
           <div>视频直播平台</div>
-        </div>
+        </div>*/}
       </div>
     )
   }
