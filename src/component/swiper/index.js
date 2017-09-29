94e0b9b8773560b16b5a79ecb7776119045c3797
GayeChen <1580850542@qq.com>
2017-09-29 13:45:31 +0800

    修改轮播图

--- src/component/swiper/index.js
+++ src/component/swiper/index.js
@@ -7,45 +7,62 @@ import "jquery"
 import "swiper/dist/css/swiper.min.css"
 import "swiper/dist/js/swiper.min.js"
 import Swiper from "swiper/dist/js/swiper.js"
-export default class SwiperMy extends React.Component{
 
-    componentWillMount(){
-
+export default class SwiperMy extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      btnSwitchVisible: false
     }
-
-
-    render(){
-        return(
-            <div className="swiper-container" ref="container">
-                <div className="swiper-wrapper">
-                    <div className="swiper-slide">
-                        <img src={img1} alt=""/>
-                    </div>
-                    <div className="swiper-slide">
-                        <img src={img2} alt=""/>
-                    </div>
-                    <div className="swiper-slide">
-                        <img src={img3} alt=""/>
-                    </div>
-                </div>
-
-                {/*<div className="swiper-pagination" ref="pagination"></div>*/}
-                <div className="swiper-button-prev swiper-button-white" ref="prev"></div>
-                <div className="swiper-button-next swiper-button-white" ref="next"></div>
-            </div>
-        )
-    }
-    componentDidMount(){
-
-            new Swiper(this.refs.container,{
-            loop:true,
-            autoplay : 3000,
-            speed:1000,
-            autoplayDisableOnInteraction : false,
-            paginationType:'fraction',
-            prevButton: this.refs.prev,
-            nextButton: this.refs.next,
-        })
-    }
-
+  }
+  
+  render() {
+    return (
+    <div
+      className="swiper-container"
+      ref="container"
+      onMouseEnter={() => this.setState({btnSwitchVisible: true})}
+      onMouseLeave={() => this.setState({btnSwitchVisible: false})}
+    >
+      <div className="swiper-wrapper">
+        <div className="swiper-slide">
+          <img src={img1} alt=""/>
+        </div>
+        <div className="swiper-slide">
+          <img src={img2} alt=""/>
+        </div>
+        <div className="swiper-slide">
+          <img src={img3} alt=""/>
+        </div>
+      </div>
+      
+      {/*<div className="swiper-pagination" ref="pagination"></div>*/}
+      {
+        this.state.btnSwitchVisible ?
+        <div>
+          <div className="swiper-button-prev swiper-button-white" ref="prev"></div>
+          <div className="swiper-button-next swiper-button-white" ref="next"></div>
+        </div>
+        :
+        null
+      }
+    </div>
+    )
+  }
+  
+  componentDidMount() {
+    
+    new Swiper(this.refs.container, {
+      loop: true,
+      effect: 'coverflow',
+      autoplay: 3000,
+      grabCursor: true,
+      speed: 1000,
+      autoplayDisableOnInteraction: false,
+      // paginationType: 'fraction',
+      prevButton: this.refs.prev,
+      nextButton: this.refs.next,
+    })
+  }
+  
 }
