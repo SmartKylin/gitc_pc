55007132e4f7bcbe676ee7b62fd58a7411246a69
GayeChen <1580850542@qq.com>
2017-11-17 18:00:35 +0800

    修改大会日程嘉宾排序

--- src/services/home.js
+++ src/services/home.js
@@ -6,11 +6,11 @@ export function getPeopleList(pageId) {
 }
 
 export function getDate1() {
-  return http.get('http://120.92.10.182:8000/api/gitc/person-4/all/list.json?token=1afb756d16740266efde290917ca1a8e&type=1&phone=18201440272')
+  return http.get('http://120.92.10.182:8000/api/gitc/person-4/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=stime')
 }
 
 export function getDate2() {
-  return http.get('http://120.92.10.182:8000/api/gitc/person-6/all/list.json?token=1afb756d16740266efde290917ca1a8e&type=1&phone=18201440272')
+  return http.get('http://120.92.10.182:8000/api/gitc/person-6/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=stime')
 }
 
 export function getSpeecherList() {
