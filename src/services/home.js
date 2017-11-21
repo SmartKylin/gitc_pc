0b870579f5644d3ca7ef191b9a957dbf97e10d3d
GayeChen <1580850542@qq.com>
2017-11-21 15:34:25 +0800

    大会议程排序权重

--- src/services/home.js
+++ src/services/home.js
@@ -6,11 +6,11 @@ export function getPeopleList(pageId) {
 }
 
 export function getDate1() {
-  return http.get('http://120.92.10.182:8000/api/gitc/person-4/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=stime')
+  return http.get('http://120.92.10.182:8000/api/gitc/person-4/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=-weight')
 }
 
 export function getDate2() {
-  return http.get('http://120.92.10.182:8000/api/gitc/person-6/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=stime')
+  return http.get('http://120.92.10.182:8000/api/gitc/person-6/all/list.json?token=1afb756d16740266efde290917ca1a8e&phone=18201440272&order_by=-weight')
 }
 
 export function getSpeecherList() {
